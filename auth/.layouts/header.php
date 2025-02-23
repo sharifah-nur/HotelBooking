eb4c963e3e9c4034affa9c728aaf63951be9d6a0
sharifah-nur <hadiyasharifahnur@gmail.com>
2025-02-23 18:14:45 +0000

    menambahkan kode validasi login dan akses dashboard pada auth/.layouts/header.php

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -1,3 +1,20 @@
+<?php
+session_start(); //inisialisasi session
+//ambil notifikasi jika ada, kemudian hapus dari sesi
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION['notification']);
+}
+/* jika user sudah login sesuai dengan username atau role maka akan diarahkan ke URL: dashboard.php*/
+if (isset($_SESSION["username"]) || isset($_SESSION["role"])) {
+  $_SESSION['notification'] = [
+    'type' => 'danger',
+    'message' => 'Silahkan Logout Terlebih Dahulu!'
+  ];
+  header('location: ../dashboard.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
