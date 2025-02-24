f3f170f086624c453a2c34497e2ce52003ac4ef1
farra-fawnia <farrafawnia01@gmail.com>
2025-02-24 10:31:01 +0700

    mengubah tampilan side menu

--- .includes/sidemenu.php
+++ .includes/sidemenu.php
@@ -2,7 +2,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
   <div class="app-brand demo">
     <a href="./dashboard.php" class="app-brand-link">
-      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">Hotel Booking</span>
+      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">HooKing</span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>
